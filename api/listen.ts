64e6068e10e6881bc88e00a087c3fc123d752817
0xShuk <83659045+0xShuk@users.noreply.github.com>
2023-12-19 00:48:59 +0530

    update 12

--- api/listen.ts
+++ api/listen.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import mongoose from "mongoose";
+import { text } from 'node:stream/consumers';
 import Transaction from "../models/tx";
 import parseData from "../utils/anchor";
 
@@ -11,33 +12,33 @@ try {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.headers.authorization && req.headers.authorization === process.env.HELIUS_HEADER) {
-    const txObject = req.body[0];
-    txObject.meta.innerInstructions.forEach(async(ix) => {
-      ix.instructions.forEach(async(inIx) => {
-        console.log(txObject.transaction.message.accountKeys[inIx.programIdIndex])
-
-        if (txObject.transaction.message.accountKeys[inIx.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
-          const parsedData = parseData(inIx.data);
-          const transaction = new Transaction({
-            params: JSON.stringify(parsedData?.data),
-            sig: JSON.stringify(txObject.transaction.signatures),
-            accounts: inIx.accounts.map(num => txObject.transaction.message.accountKeys[num]),
-            type: parsedData?.name
-          });
-
-          try {
-            await transaction.save();
-          } catch(e) {
-            res.status(403).send(e)
+    const txObject = req.body;
+    txObject.forEach(tx => {
+      tx.meta.innerInstructions.forEach(async(ix) => {
+        ix.instructions.forEach(async(inIx) => {  
+          if (tx.transaction.message.accountKeys[inIx.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
+            const parsedData = parseData(inIx.data);
+            const transaction = new Transaction({
+              params: JSON.stringify(parsedData?.data),
+              sig: JSON.stringify(tx.transaction.signatures),
+              accounts: inIx.accounts.map(num => tx.transaction.message.accountKeys[num]),
+              type: parsedData?.name
+            });
+  
+            try {
+              await transaction.save();
+            } catch(e) {
+              res.status(403).send(e)
+            }
+          } else {
+            return res.json({
+              message: `Work Half Done!`,
+            })
           }
-        } else {
-          console.log(txObject.transaction.message.accountKeys[inIx.programIdIndex])
-          return res.json({
-            message: `Work Half Done!`,
-          })
-        }
-      })
-    });
+        })
+      });
+    })
+    
 
     return res.json({
       message: `Work Done!`,
