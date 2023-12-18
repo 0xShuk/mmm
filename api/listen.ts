70b1117118b14c2eef241031c7a41c9ef38da8e8
0xShuk <83659045+0xShuk@users.noreply.github.com>
2023-12-19 01:05:09 +0530

    update 14

--- api/listen.ts
+++ api/listen.ts
@@ -14,38 +14,31 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.headers.authorization && req.headers.authorization === process.env.HELIUS_HEADER) {
     const txObject = req.body;
     txObject.forEach(tx => {
-      console.log(tx)
-      // tx.meta.innerInstructions.forEach(async(ix) => {
-      //   ix.instructions.forEach(async(inIx) => {  
-      //     if (tx.transaction.message.accountKeys[inIx.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
-      //       const parsedData = parseData(inIx.data);
-      //       const transaction = new Transaction({
-      //         params: JSON.stringify(parsedData?.data),
-      //         sig: JSON.stringify(tx.transaction.signatures),
-      //         accounts: inIx.accounts.map(num => tx.transaction.message.accountKeys[num]),
-      //         type: parsedData?.name
-      //       });
-  
-      //       try {
-      //         await transaction.save();
-      //       } catch(e) {
-      //         res.status(403).send(e)
-      //       }
-      //     } else {
-      //       return res.json({
-      //         message: `Work Half Done!`,
-      //       })
-      //     }
-      //   })
-      // });
-    })
-    
+      const message = tx.transaction.message;
+      message.instructions.forEach(async(ix) => {
+        if (message.accountKeys[ix.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
+          const parsedData = parseData(ix.data);
+          const transaction = new Transaction({
+            params: JSON.stringify(parsedData?.data),
+            sig: JSON.stringify(tx.transaction.signatures),
+            accounts: ix.accounts.map(num => message.accountKeys[num]),
+            type: parsedData?.name
+          });
+
+          await transaction.save();
+          return res.json({
+            message: `Work Done!`,
+          })
+        }
+      })
+    });
 
     return res.json({
-      message: `Work Done!`,
+      message: `Not found!`,
+    })
+  } else {
+    return res.json({
+      message: `Hello bearer!`,
     })
   }
-  return res.json({
-    message: `Hello bearer!`,
-  })
 }
