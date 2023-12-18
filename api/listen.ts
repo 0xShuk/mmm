81484ff300b90c367763aedd8640bbbd11710a35
0xShuk <83659045+0xShuk@users.noreply.github.com>
2023-12-19 00:52:13 +0530

    update 13

--- api/listen.ts
+++ api/listen.ts
@@ -14,29 +14,30 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.headers.authorization && req.headers.authorization === process.env.HELIUS_HEADER) {
     const txObject = req.body;
     txObject.forEach(tx => {
-      tx.meta.innerInstructions.forEach(async(ix) => {
-        ix.instructions.forEach(async(inIx) => {  
-          if (tx.transaction.message.accountKeys[inIx.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
-            const parsedData = parseData(inIx.data);
-            const transaction = new Transaction({
-              params: JSON.stringify(parsedData?.data),
-              sig: JSON.stringify(tx.transaction.signatures),
-              accounts: inIx.accounts.map(num => tx.transaction.message.accountKeys[num]),
-              type: parsedData?.name
-            });
+      console.log(tx)
+      // tx.meta.innerInstructions.forEach(async(ix) => {
+      //   ix.instructions.forEach(async(inIx) => {  
+      //     if (tx.transaction.message.accountKeys[inIx.programIdIndex] === "mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc") {
+      //       const parsedData = parseData(inIx.data);
+      //       const transaction = new Transaction({
+      //         params: JSON.stringify(parsedData?.data),
+      //         sig: JSON.stringify(tx.transaction.signatures),
+      //         accounts: inIx.accounts.map(num => tx.transaction.message.accountKeys[num]),
+      //         type: parsedData?.name
+      //       });
   
-            try {
-              await transaction.save();
-            } catch(e) {
-              res.status(403).send(e)
-            }
-          } else {
-            return res.json({
-              message: `Work Half Done!`,
-            })
-          }
-        })
-      });
+      //       try {
+      //         await transaction.save();
+      //       } catch(e) {
+      //         res.status(403).send(e)
+      //       }
+      //     } else {
+      //       return res.json({
+      //         message: `Work Half Done!`,
+      //       })
+      //     }
+      //   })
+      // });
     })
     
 
